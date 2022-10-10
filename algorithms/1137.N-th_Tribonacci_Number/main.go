61c1d9271cab728841225e88ae3e42dd5df5537c
patricelee <patrice.lee@mlytics.com>
2022-10-10 16:28:18 +0800

    1137.N-th_Tribonacci_Number

---
+++ algorithms/1137.N-th_Tribonacci_Number/main.go
@@ -0,0 +1,29 @@
+package main
+
+// The Tribonacci sequence Tn is defined as follows:
+// T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+// Given n, return the value of Tn.
+
+// Example 1:
+// Input: n = 4
+// Output: 4
+// Explanation:
+// T_3 = 0 + 1 + 1 = 2
+// T_4 = 1 + 1 + 2 = 4
+
+// Example 2:
+// Input: n = 25
+// Output: 1389537
+
+func tribonacci(n int) int {
+	a, b, c := 0, 1, 1
+	if n < 2 {
+		return n
+	}
+
+	for i := 3; i <= n; i++ {
+		sum := a + b + c
+		a, b, c = b, c, sum
+	}
+	return c
+}
